f791b54e81532db1a3f13ba6026b5fd2626ec900
Marek Vicha <vicha-marek@seznam.cz>
2024-02-28 16:00:29 +0100

    shared_prefs

---
+++ lib/utils/shared_prefs.dart
@@ -0,0 +1,11 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefs {
+  late final SharedPreferences _sharedPrefs;
+
+  Future<void> init() async {
+    _sharedPrefs = await SharedPreferences.getInstance();
+  }
+
+}
+final sharedPrefs = SharedPrefs();
